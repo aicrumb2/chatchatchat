89669c5f1158c6dc4cf0d7b4a621578a35b02e44
bunnybelle84 <99917711+bunnybelle84@users.noreply.github.com>
2022-02-17 16:23:08 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -3,29 +3,22 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
+// valid rooms are 0, 1, 2, 3
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
   
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+  socket.on('chat message', data => {
+    io.emit('chat message', data);
   });
 
-  socket.on('orange', msg => {
-    io.emit('orange', msg); // i know there's a way to do this with just chat message but idfk how and im lazy
+  socket.on('orange', data => {
+    io.emit('orange', data); // i know there's a way to do this with just chat message but idfk how and im lazy
   });
 
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-
-  // THE GAME STUFF
-  socket.on('position', (data) => {
-    socket.broadcast.emit('position', data);
-  });
 });
 
 http.listen(port, () => {
