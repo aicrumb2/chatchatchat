3482ebd26796869286b5b76f774677baa8499bdd
bunnybelle84 <99917711+bunnybelle84@users.noreply.github.com>
2022-02-17 14:33:23 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const fs = require('fs');
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+    fs.writeFile('log.txt', "\n"+msg, { flag: 'a+' }, err => {})
+  });
+
+  socket.on('orange', msg => {
+    io.emit('orange', msg);
+    fs.writeFile('log.txt', "\n"+msg, { flag: 'a+' }, err => {})
+  });
+
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(port, () => {
+  console.log(`server running at http://localhost:${port}/`);
+});
