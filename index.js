1db16cff6214ec6ac3029ae7c1f085fdcf43a650
bunnybelle84 <99917711+bunnybelle84@users.noreply.github.com>
2022-02-17 16:36:09 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-// valid rooms are 0, 1, 2, 3
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -13,10 +11,6 @@ io.on('connection', (socket) => {
   
   socket.on('chat message', data => {
     io.emit('chat message', data);
-  });
-
-  socket.on('orange', data => {
-    io.emit('orange', data); // i know there's a way to do this with just chat message but idfk how and im lazy
   });
 
 });
